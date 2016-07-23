05833f376e38319bef4de92d92041c01054acc25
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2016-07-23 14:52:09 +0900

    フロント作成

--- js/function.js
+++ js/function.js
@@ -1,9 +1,24 @@
 $(function(){
+  $('#output').css({
+    display:'none'
+  });
+
+  var $input = $('input'),
+      user = {
+        name : 'name',
+        birthday : 19700101
+      };
 
   $('#submit').on("click",function(e){
-    //各要素を取り出す
-    console.log($('input'));
-    //ここに
+    //各要素を取りしてuserに反映
+    user.name = $input[0].value;
+    user.birthday =  $input[1].value;
+    //alert('本当にいいんですか？');
+    //alert('本当の本当にいいんですか？');
+    //画面切り替え
+    $('#input').toggle();
+    $('#output').toggle();
+
   });
 
 });
